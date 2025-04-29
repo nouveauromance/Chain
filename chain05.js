a18175141846f737417f97115be0497fba1161e8
Natali Mendonça Dau <natalidau@gmail.com>
2025-04-29 08:32:26 -0300

    update

--- chain05.js
+++ chain05.js
@@ -11,52 +11,73 @@ class ValidadorHandler{
         this.proximaEtapa = proximaEtapa;
     }
 
-    handleValidador(validador){
+    handleValidador(validador, valor){
         throw new Error("Este método deve ser implementado pelas subclasses.");
     }
 }
 
 // sistemas de validação concretos
+function validarEmail(email){
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+   return regexEmail.test(email)
+}
+
+function validarSenha(senha){
+    const regexSenha = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return regexSenha.test(senha);
+}
+
 class ValidadorEmail extends ValidadorHandler{
-    handleValidador(validador){
-        if (validador.formato === "email") {
-            console.log("[--OK--] Formato de formulário verificado. Sucesso.");
-        } else {
-            console.log("[--FAIL-] Formato inválido. Operação encerrada.");
+    handleValidador(validador, valor){
+        if (validador === "email") {
+            if (validarEmail(valor)) {
+                console.log("[--OK--] Formato de formulário verificado. Sucesso.");
+            } else {
+                console.log("[--FAIL-] Formato inválido. Operação encerrada.");
+            }
         }
     }
 }
 
-function validarSenha(senha) {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    return regex.test(senha);
-} 
-
-function validarUsername(user) {
-    const lista = [user1234, user1111, user2222, user3333, user4444];
-    return lista.test(user);
-} 
-
 class ValidadorSenha extends ValidadorHandler{
-    handleValidador(validador){
-        console.log("[--WAIT--] Verificando...");
-        if (validador.formato === "senha") {
-            validarSenha(validador.formato)
+    handleValidador(validador, valor){
+        if (validador === "senha") {
+            if (validarSenha(valor)) {
                 console.log("[--OK--] Formato válido.")
-        } else {
-            console.log("[--FAIL-] Senha inválida. Operação encerrada.");
+            } else {
+                console.log("[--FAIL-] Senha inválida. Operação encerrada.");
+            }
         }
     }
 }
 
 class ValidadorUsername extends ValidadorHandler{
-    handleValidador(validador){
-        console.log("[--WAIT--] Verificando...");
-        if (validador.formato === "username") {
-            console.log("[--OK--] Formato válido.")
-        } else {
-            validarUsername(validador.formato)
-            console.log("[--FAIL-] Username inválido. Operação encerrada.")
+    handleValidador(validador, valor){
+        if (validador === "username") {
+            if (validarUsername(valor)) {
+                console.log("[--OK--] Formato válido.")
+            } else {
+                validarUsername(validador.formato)
+                console.log("[--FAIL-] Username inválido. Operação encerrada.")
+            }
         }
     }
 }
+
+// uso
+const usuariosRegistrados = ["usuario123", "usuarioFATEC", "usuario777"]
+
+function validarUsername(user){
+    return !usuariosRegistrados.includes(user);
+}
+ 
+const validaEmail = new ValidadorEmail();
+const validaSenha = new ValidadorSenha();
+const validaUsername = new ValidadorUsername();
+ 
+validaEmail.setProximaEtapa(validaSenha);
+validaSenha.setProximaEtapa(validaUsername);
+ 
+console.log(validaEmail.handleValidador("email", "emailTeste@gmail.com"));
+console.log(validaSenha.handleValidador("senha", "SenhaTeste158"))
+console.log(validaUsername.handleValidador("username", "Nat579"));
